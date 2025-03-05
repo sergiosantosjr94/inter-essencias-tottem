71caaa047542f94200e0973a8d17c5740397d0a9
sergiosantosjr94 <sllsjunior@gmail.com>
2025-03-05 15:15:25 -0300

    Add: images and products to database

--- database/perfumes.tsx
+++ database/perfumes.tsx
@@ -123,13 +123,213 @@ export const masculinos: IPerfumes[] = [
   },
   {
     id: 12,
-    img: "/perfumes/masculinos/ch-men.webp",
-    title: "CH MEN",
-    brand: "Carolina Herrera",
+    img: "/perfumes/masculinos/victory-elixir.webp",
+    title: "Invictus Victory Elixir",
+    brand: "Paco Rabanne",
     description:
-      "CH Men de Carolina Herrera é um perfume Oriental Especiado Masculino. CH Men foi lançada em 2009. As notas de topo são: Grama, Bergamota e Toranja. As notas de coração são: Notas Amadeiradas, Noz-moscada, Violeta, Açafrão e Jasmim. As notas de fundo são: Açúcar, Couro, Baunilha, Camurça, Âmbar, Madeira de Cashmir, Sândalo, Musgo de Carvalho e Vetiver.",
-    acordes: "/perfumes/masculinos/acordes/ch-men-acordes.png",
-    notas: "/perfumes/masculinos/notas/ch-men-notas.png",
+      "Invictus Victory Elixir de Rabanne é um perfume Oriental Amadeirado Masculino. Esta é uma nova fragrância. Invictus Victory Elixir foi lançada em 2023. As notas de topo são: Lavanda, Cardamomo e Pimenta Preta. As notas de coração são: Incenso e Patchouli. As notas de fundo são: Fava de Baunilha e Fava Tonka.",
+    acordes: "/perfumes/masculinos/acordes/victory-elixir-acordes.png",
+    notas: "/perfumes/masculinos/notas/victory-elixir-notas.png",
+  },
+  {
+    id: 13,
+    img: "/perfumes/masculinos/versace-pour-homme.webp",
+    title: "Versace Pour Homme",
+    brand: "Versace",
+    description:
+      "Versace Pour Homme de Versace é um perfume Aromático Fougére Masculino. Versace Pour Homme foi lançada em 2008. O perfumista que assina esta fragrância é Alberto Morillas. As notas de topo são: Limão, Bergamota, Néroli e Rosa Centifolia. As notas de coração são: Jacinto, Cedro, Sálvia Esclaréia e Gerânio. As notas de fundo são: Fava Tonka, Almíscar e Âmbar.",
+    acordes: "/perfumes/masculinos/acordes/versace-pour-homme-acordes.png",
+    notas: "/perfumes/masculinos/notas/versace-pour-homme-notas.png",
+  },
+  {
+    id: 14,
+    img: "/perfumes/masculinos/armani-code.webp",
+    title: "Armani Code",
+    brand: "Armani",
+    description:
+      "Armani Code de Giorgio Armani é um perfume Oriental Especiado Masculino. Armani Code foi lançada em 2004. O perfumista que assina esta fragrância é Antoine Lie. As notas de topo são: Limão e Bergamota. As notas de coração são: Anis Estrelado, Flor de Oliveira e Madeira Guaiac. As notas de fundo são: Couro, Fava Tonka e Tabaco. Este Perfume é premiado FiFi Award Fragrance Of The Year Men`s Luxe 2006.",
+    acordes: "/perfumes/masculinos/acordes/armani-code-acordes.png",
+    notas: "/perfumes/masculinos/notas/armani-code-homme-notas.png",
+  },
+  {
+    id: 15,
+    img: "/perfumes/masculinos/dolce-e-gabanna.webp",
+    title: "Pour Homme",
+    brand: "Dolce & Gabanna",
+    description:
+      "Dolce&Gabbana Pour Homme (2012) de Dolce&Gabbana é um perfume Aromático Fougére Masculino. Dolce&Gabbana Pour Homme (2012) foi lançada em 2012. As notas de topo são: Cítricos, Bergamota, Néroli e Mandarina. As notas de coração são: Lavanda, Sálvia e Pimenta. As notas de fundo são: Tabaco, Fava Tonka e Cedro.",
+    acordes: "/perfumes/masculinos/acordes/dolce-e-gabana-acordes.png",
+    notas: "/perfumes/masculinos/notas/dolce-e-gabana-notas.png",
+  },
+  {
+    id: 16,
+    img: "/perfumes/masculinos/bvlgari-man-in-black.webp",
+    title: "Bvlgari Man In Black",
+    brand: "Bvlgari",
+    description:
+      "Bvlgari Man In Black de Bvlgari é um perfume Oriental Floral Masculino. Bvlgari Man In Black foi lançada em 2014. O perfumista que assina esta fragrância é Alberto Morillas. As notas de topo são: Especiarias, Rum e Tabaco. As notas de coração são: Couro, Íris e Tuberosa. As notas de fundo são: Fava Tonka, Madeira Guaiac e Benjoim.",
+    acordes: "/perfumes/masculinos/acordes/bvlgari-man-in-black-acordes.png",
+    notas: "/perfumes/masculinos/notas/bvlgari-man-in-black-notas.png",
+  },
+  {
+    id: 17,
+    img: "/perfumes/masculinos/fahrenheit.webp",
+    title: "Fahrenheit",
+    brand: "Christian Dior",
+    description:
+      "Fahrenheit de Dior é um perfume Aromático Fougére Masculino. Fahrenheit foi lançada em 1988. Fahrenheit foi criado por Jean-Louis Sieuzac, Michel Almairac e Maurice Roger. As notas de topo são: Flor de Noz-Moscada, Lavanda, Cedro, Mandarina, Camomila, Espinheiro branco, Bergamota e Limão. As notas de coração são: Folha de Violeta, Noz-moscada, Cedro, Sândalo, Cravo, Madressilva, Jasmim e Lírio-do-Vale. As notas de fundo são: Couro, Vetiver, Almíscar, Âmbar, Patchouli e Fava Tonka.",
+    acordes: "/perfumes/masculinos/acordes/fahrenheit-acordes.png",
+    notas: "/perfumes/masculinos/notas/fahrenheit-notas.png",
+  },
+  {
+    id: 18,
+    img: "/perfumes/masculinos/ferrari-black.webp",
+    title: "Ferrari Black",
+    brand: "Scuderia Ferrari",
+    description:
+      "Ferrari Black de Ferrari é um perfume Aromático Fougére Masculino. Ferrari Black foi lançada em 1999. As notas de topo são: Maçã Vermelha, Ameixa, Lima e Bergamota. As notas de coração são: Canela, Jasmim, Rosa e Cardamomo. As notas de fundo são: Baunilha, Âmbar, Cedro e Almíscar.",
+    acordes: "/perfumes/masculinos/acordes/ferrari-black-acordes.png",
+    notas: "/perfumes/masculinos/notas/ferrari-black-notas.png",
+  },
+  {
+    id: 19,
+    img: "/perfumes/masculinos/fierce.webp",
+    title: "Fierce",
+    brand: "Abercrombie e Fitch",
+    description:
+      "Fierce de Abercrombie & Fitch é um perfume Amadeirado Aromático Masculino. Fierce foi lançada em 2002. Fierce foi criado por Christophe Laudamiel e Bruno Jovanovic. As notas de topo são: Abeto, Limão, Laranja, Petitgrain, Cardamomo e Notas Oceânicas. As notas de coração são: Alecrim, Jasmim, Lírio-do-Vale, Rosa e Sálvia. As notas de fundo são: Almíscar, Vetiver, Musgo de Carvalho, Pau-Rosa e Sândalo.",
+    acordes: "/perfumes/masculinos/acordes/fierce-acordes.png",
+    notas: "/perfumes/masculinos/notas/fierce-notas.png",
+  },
+  {
+    id: 20,
+    img: "/perfumes/masculinos/invictus.webp",
+    title: "Invictus",
+    brand: "Paco Rabanne",
+    description:
+      "Invictus de Rabanne é um perfume Aquático Amadeirado Masculino. Invictus foi lançada em 2013. Invictus foi criado por Veronique Nyberg, Anne Flipo, Olivier Polge e Dominique Ropion. As notas de topo são: Notas Oceânicas, Toranja e Mandarina. As notas de coração são: Folha de Louro e Jasmim. As notas de fundo são: Âmbar Cinzento, Madeira Guaiac, Musgo de Carvalho e Patchouli.",
+    acordes: "/perfumes/masculinos/acordes/invictus-acordes.png",
+    notas: "/perfumes/masculinos/notas/invictus-notas.png",
+  },
+  {
+    id: 21,
+    img: "/perfumes/masculinos/kouros.webp",
+    title: "Kouros",
+    brand: "Yves Saint Laurent",
+    description:
+      "Kouros de Yves Saint Laurent é um perfume Aromático Fougére Masculino. Kouros foi lançada em 1981. O perfumista que assina esta fragrância é Pierre Bourdon. As notas de topo são: Aldeídos, Coentro, Sálvia Esclaréia, Artemísia e Bergamota. As notas de coração são: Patchouli, Cravo, Gerânio, Vetiver, Jasmim, Canela, Raíz de Orris e Lavanda. As notas de fundo são: Civeta, Mel, Couro, Almíscar, Musgo de Carvalho, Âmbar, Fava Tonka e Baunilha.",
+    acordes: "/perfumes/masculinos/acordes/kouros-acordes.png",
+    notas: "/perfumes/masculinos/notas/kouros-notas.png",
+  },
+  {
+    id: 22,
+    img: "/perfumes/masculinos/leau-dissey.webp",
+    title: "L'eau Dissey Pour Homme",
+    brand: "Issey Miyake",
+    description:
+      "L’Eau d’Issey Pour Homme de Issey Miyake é um perfume Aquático Amadeirado Masculino. L’Eau d’Issey Pour Homme foi lançada em 1994. O perfumista que assina esta fragrância é Jacques Cavallier Belletrud. As notas de topo são: Yuzu, Limão, Bergamota, Lúcia-lima, Mandarina, Cipreste, Calone, Coentro, Estragão e Sálvia. As notas de coração são: Lótus azul, Noz-moscada, Lírio-do-Vale, Gerânio Bourbon, Açafrão, Canela do Ceilão e Resedá. As notas de fundo são: Vetiver do Tahiti, Almíscar, Cedro, Sândalo, Âmbar e Tabaco.",
+    acordes: "/perfumes/masculinos/acordes/leau-dissey-acordes.png",
+    notas: "/perfumes/masculinos/notas/leau-dissey-notas.png",
+  },
+  {
+    id: 23,
+    img: "/perfumes/masculinos/y.webp",
+    title: "Y",
+    brand: "Yves Saint Laurent",
+    description:
+      "Y Eau de Parfum de Yves Saint Laurent é um perfume Aromático Fougére Masculino. Y Eau de Parfum foi lançada em 2018. O perfumista que assina esta fragrância é Dominique Ropion. As notas de topo são: Maçã, Gengibre e Bergamota. As notas de coração são: Sálvia, Bagas de Zimbro e Gerânio. As notas de fundo são: Madeira de Âmbar, Fava Tonka, Cedro, Vetiver e Olíbano.",
+    acordes: "/perfumes/masculinos/acordes/y-acordes.png",
+    notas: "/perfumes/masculinos/notas/y-notas.png",
+  },
+  {
+    id: 24,
+    img: "/perfumes/masculinos/le-male.webp",
+    title: "Le Male",
+    brand: "Jean Paul Gaultier",
+    description:
+      "Le Male de Jean Paul Gaultier é um perfume Oriental Fougére Masculino. Le Male foi lançada em 1995. O perfumista que assina esta fragrância é Francis Kurkdjian. As notas de topo são: Lavanda, Hortelã, Cardamomo, Bergamota e Artemísia. As notas de coração são: Canela, Flor de Laranjeira e Alcarávia. As notas de fundo são: Baunilha, Fava Tonka, Âmbar, Sândalo e Cedro.",
+    acordes: "/perfumes/masculinos/acordes/le-male-acordes.png",
+    notas: "/perfumes/masculinos/notas/le-male-notas.png",
+  },
+  {
+    id: 25,
+    img: "/perfumes/masculinos/malbec.webp",
+    title: "Malbec",
+    brand: "Bvlgari",
+    description:
+      "Malbec de O Boticário é um perfume Amadeirado Especiado Masculino. Malbec foi lançada em 2004. O perfumista que assina esta fragrância é Napoleão Bastos. As notas de topo são: Cedro da Virgínia, Incenso, Pimenta, Lima, Mandarina, Cassis, Limão de Amalfi, Folha de Violeta, Bergamota e Cardamomo. As notas de coração são: Cedro, Patchouli e Sândalo. As notas de fundo são: Musgo de Carvalho, Âmbar, Benjoim e Almíscar.",
+    acordes: "/perfumes/masculinos/acordes/malbec-acordes.png",
+    notas: "/perfumes/masculinos/notas/malbec-notas.png",
+  },
+  {
+    id: 26,
+    img: "/perfumes/masculinos/one-million.webp",
+    title: "One Million",
+    brand: "Paco Rabanne",
+    description:
+      "1 Million é o perfume importado masculino icônico de Paco Rabanne. O maior best-seller da perfumaria masculina.<br>Seja flamejante! Aprecie os desafios da vida e mostre o seu brilho. Exiba sua singularidade, viva no limite e mostre sua excelência. Viver não significa seguir todas as regras e, com 1 Million, você tem o poder de ditá-las. Um perfume Eau de Toilette feito para homens intrigantes e cavalheiros, capazes de cativar e seduzir sem muito esforço. Inspirado na ambiguidade do frescor do couro, mostra a classe, o espírito e a paixão do homem.<br>O perfume apresenta os fantásticos aromas genuínos do couro e do âmbar como assinatura da fragrância chamativa, refrescante e picante, que impressiona desde as primeiras notas. Sua complexidade combina toques de tangerina e hortelã com um extra especiado de canela e rosas para um frescor acentuado. O aroma quente e sedutor fecha com notas amadeiradas e o patchouli.",
+    acordes: "/perfumes/masculinos/acordes/one-million-acordes.png",
+    notas: "/perfumes/masculinos/notas/one-million-notas.png",
+  },
+  {
+    id: 27,
+    img: "/perfumes/masculinos/polo-green.webp",
+    title: "Polo",
+    brand: "Ralph Lauren",
+    description:
+      "Polo de Ralph Lauren é um perfume Chipre Amadeirado Masculino. Polo foi lançada em 1978. O perfumista que assina esta fragrância é Carlos Benaïm. As notas de topo são: Bagas de Zimbro, Manjericão, Artemísia, Alcarávia, Coentro e Bergamota. As notas de coração são: Agulhas de Pinheiro, Couro, Camomila, Pimenta, Cravo, Gerânio, Jasmim e Rosa. As notas de fundo são: Tabaco, Musgo de Carvalho, Patchouli, Cedro, Vetiver, Almíscar e Âmbar.",
+    acordes: "/perfumes/masculinos/acordes/polo-acordes.png",
+    notas: "/perfumes/masculinos/notas/polo-notas.png",
+  },
+  {
+    id: 28,
+    img: "/perfumes/masculinos/polo-sport.webp",
+    title: "Polo",
+    brand: "Ralph Lauren",
+    description:
+      "Polo Sport de Ralph Lauren é um perfume Aromático Verde Masculino. Polo Sport foi lançada em 1994. O perfumista que assina esta fragrância é Harry Fremont. As notas de topo são: Hortelã, Aldeídos, Lavanda, Bergamota, Mandarina, Limão, Artemísia e Néroli. As notas de coração são: Alga Marinha, Gengibre, Jasmim, Gerânio, Cyclamen, Pau-Rosa e Rosa. As notas de fundo são: Almíscar, Sândalo, Cedro, Madeira Guaiac e Âmbar.",
+    acordes: "/perfumes/masculinos/acordes/polo-sport-acordes.png",
+    notas: "/perfumes/masculinos/notas/polo-sport-notas.png",
+  },
+  {
+    id: 29,
+    img: "/perfumes/masculinos/sauvage.webp",
+    title: "Sauvage",
+    brand: "Christian Dior",
+    description:
+      "Sauvage é um ato de criação inspirado em grandes espaços abertos. Uma trilha poderosa e fresca, rústica e nobre ao mesmo tempo.<br>Sauvage Eau de Parfum é intenso e suave, noturno e fresco, misteriosamente poderoso. A bergamota da Calábria adiciona uma assinatura suculenta à fragrância. Enquanto a noz-moscada da Indonésia acompanha a pimenta de Sichuan para criar notas características do Sauvage. Por sua vez, o rastro ambarado amadeirado do Ambroxan® envolve o absoluto de baunilha, juntamente com toques esfumados que exaltam sua virilidade.<br>François Demachy compôs as fragrâncias Sauvage como uma ode à natureza. Graças a parcerias ao redor do mundo, ele tem sido capaz de selecionar apenas as matérias-primas mais excepcionais produzidas com produtores locais para fornecer uma assinatura sob medida para a Maison Dior. Descubra no episódio de Born in the Wild centrado no Sauvage Eau de Parfum e seu principal ingrediente: Patchouli da Indonésia.",
+    acordes: "/perfumes/masculinos/acordes/sauvage-acordes.png",
+    notas: "/perfumes/masculinos/notas/sauvage-notas.png",
+  },
+  {
+    id: 30,
+    img: "/perfumes/masculinos/silver-scent.webp",
+    title: "Silver Scent",
+    brand: "Jacques Bogart",
+    description:
+      "Silver Scent de Jacques Bogart é um perfume Oriental Amadeirado Masculino. Silver Scent foi lançada em 2006. As notas de topo são: Flor de Laranjeira e Limão. As notas de coração são: Lavanda, Cardamomo, Noz-moscada, Alecrim, Coentro e Gerânio. As notas de fundo são: Lichia, Fava Tonka, Madeira de Teca e Vetiver.",
+    acordes: "/perfumes/masculinos/acordes/silver-scent-acordes.png",
+    notas: "/perfumes/masculinos/notas/silver-scent-notas.png",
+  },
+  {
+    id: 31,
+    img: "/perfumes/masculinos/the-one.webp",
+    title: "The One",
+    brand: "Dolce & Gabanna",
+    description:
+      "The One for Men Eau de Parfum de Dolce&Gabbana é um perfume Amadeirado Especiado Masculino. The One for Men Eau de Parfum foi lançada em 2015. O perfumista que assina esta fragrância é Olivier Polge. As notas de topo são: Toranja, Coentro e Manjericão. As notas de coração são: Cardamomo, Gengibre e Flor de Laranjeira. As notas de fundo são: Âmbar, Tabaco e Cedro.",
+    acordes: "/perfumes/masculinos/acordes/the-one-acordes.png",
+    notas: "/perfumes/masculinos/notas/the-one-notas.png",
+  },
+  {
+    id: 31,
+    img: "/perfumes/masculinos/the-one.webp",
+    title: "The One",
+    brand: "Dolce & Gabanna",
+    description:
+      "The One for Men Eau de Parfum de Dolce&Gabbana é um perfume Amadeirado Especiado Masculino. The One for Men Eau de Parfum foi lançada em 2015. O perfumista que assina esta fragrância é Olivier Polge. As notas de topo são: Toranja, Coentro e Manjericão. As notas de coração são: Cardamomo, Gengibre e Flor de Laranjeira. As notas de fundo são: Âmbar, Tabaco e Cedro.",
+    acordes: "/perfumes/masculinos/acordes/the-one-acordes.png",
+    notas: "/perfumes/masculinos/notas/the-one-notas.png",
   },
 ];
 
